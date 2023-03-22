3c60c9a8b91e48b5358f72b54676e42148b6e561
Leonardo Velasquez <lvelasquez@lemontech.com>
2023-03-22 10:57:35 -0300

    Querying for Data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,4 +12,12 @@ export class UsersService {
 
     return this.repo.save(user);
   }
+
+  findOne(id: number) {
+    return this.repo.findOne({ where: { id } });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
 }
